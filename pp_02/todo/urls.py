bab3ff33699fc376a4b36cc3c207480587078289
Jessika R <jrockel@wlan-141-23-97-75.tubit.tu-berlin.de>
2016-05-11 15:08:45 +0200

    fixed links

--- pp_02/todo/urls.py
+++ pp_02/todo/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.todo_list, name='todo_list'),
-    url(r'^$', views.todo_edit, name='todo_edit'),
-    url(r'^$', views.todo_add, name='todo_add'),
-    url(r'^$', views.todo_impr, name='todo_impr'),
+    url(r'^todo_list$', views.todo_list, name='todo_list'),
+    url(r'^todo_edit$', views.todo_edit, name='todo_edit'),
+    url(r'^todo_add$', views.todo_add, name='todo_add'),
+    url(r'^todo_impr$', views.todo_impr, name='todo_impr'),
 ]
