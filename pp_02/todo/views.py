3ddd8f8ce067ab0a688476c35d5bfbae1a54ffbd
Jessika R <jrockel@Jessikas-Air.fritz.box>
2016-05-11 19:45:41 +0200

    added functionality to list page

--- pp_02/todo/views.py
+++ pp_02/todo/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Todo
 
 def todo_list(request):
-    return render(request, 'todo/todo_list.html', {})
+    todos = Todo.objects.all
+    return render(request, 'todo/todo_list.html', {'todos' : todos})
 
 def todo_edit(request):
     return render(request, 'todo/todo_edit.html', {})
